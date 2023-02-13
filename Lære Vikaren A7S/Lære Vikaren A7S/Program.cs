202cd90a6b0c8c473e8ab10451244f93b1a9fe38
Andreas Barbesgaard <Abarbesgaard@gmail.com>
2023-02-13 12:52:44 +0100

    test af ny branch

--- Lære Vikaren A7S/Lære Vikaren A7S/Program.cs
+++ Lære Vikaren A7S/Lære Vikaren A7S/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Lære_Vikaren_A7S
+﻿using System.ComponentModel;
+
+namespace Lære_Vikaren_A7S
 {
     internal class Program
     {
@@ -8,8 +10,61 @@
          */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hva så! eller hvad?");
+            Console.WriteLine("hvilken skole har brug for vikarer: ");
+
+
+            string input = Console.ReadLine();
+            string answer = input.ToUpper();
+
+            switch(answer)
+            {
+                case "BØGELUND":
+                    Console.WriteLine("dette er en skole");
+                    break;
+                default:
+                    Console.WriteLine("Error 365");
+                    break;
+            }
+
+            
+
+
+
+
+            //afslut koden ved at trykke på 'enter'
             Console.ReadLine();
         }
+    }
+
+    public class Student
+    {
+        public string studentName;
+        
+    }
+    public class Emre : Student 
+    { 
+        
+        StudentID newStudentID = new StudentID();
+        public int studentID() 
+        {
+            return  newStudentID.RandomNR();
+        }
+
+
+    }
+    public class Alma : Student { }
+    public class Justin : Student { }
+    public class laurits : Student { }
+    public class Frederik : Student { }
+    public class Andreas : Student { }
+
+    public class StudentID 
+    { 
+     Random rd = new();
+            public int RandomNR()
+            {
+            int newRD = rd.Next(1, 1000000);
+            return newRD;
+            }
     }
 }
