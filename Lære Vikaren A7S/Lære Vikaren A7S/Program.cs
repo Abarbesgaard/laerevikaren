443c1cf4d758d3cff15f04b20903999a229f9e2d
Andreas Barbesgaard <Abarbesgaard@gmail.com>
2023-02-13 11:59:51 +0100

    note tilføjet i VS

--- Lære Vikaren A7S/Lære Vikaren A7S/Program.cs
+++ Lære Vikaren A7S/Lære Vikaren A7S/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        /* Comment: Andreas
+         * Jeg tænker at dette vil være hovedfilen for Lærervikaren A/S
+         * 
+         * 
+         * 
+         * 
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hva så! eller hvad?");
         }
     }
 }
